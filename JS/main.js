4b53d7fcaf23571423adc142c753a2026f033d34
Joaquin <bistottojoaquin13@gmail.com>
2025-06-03 13:09:25 -0300

    UPGRADES

--- JS/main.js
+++ JS/main.js
@@ -17,3 +17,20 @@ document.addEventListener('DOMContentLoaded', () => {
     appearOnScroll.observe(fader);
   });
 });
+
+/* BOTON HAMBURGUESA */
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById('menu-toggle');
+    const navList = document.getElementById('headerNavList');
+
+    toggle.addEventListener('click', () => {
+      navList.classList.toggle('active');
+    });
+
+    // OPCIONAL: Cierra el menú al hacer clic en un enlace
+    document.querySelectorAll('#headerNavList a').forEach(link => {
+      link.addEventListener('click', () => {
+        navList.classList.remove('active');
+      });
+    });
+  });
